d31298b83e699e72e44eab459bb80196712d29a9
Razaul Munir <razaulmunir@gmail.com>
2019-12-27 15:42:52 +0600

    play music with mediaPlayer

--- app/src/main/java/org/myself/servicelockscreenprcapp/MainActivity.java
+++ app/src/main/java/org/myself/servicelockscreenprcapp/MainActivity.java
@@ -1,13 +1,41 @@
 package org.myself.servicelockscreenprcapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    MediaPlayer mediaPlayer;
+
+    Button btn_play, btn_stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.a);
+
+        btn_play = findViewById(R.id.btn_play);
+        btn_stop = findViewById(R.id.btn_stop);
+
+        btn_play.setOnClickListener(this);
+        btn_stop.setOnClickListener(this);
+    }
+
+    public void onClick(View view){
+        if(view==btn_play){
+            mediaPlayer.start();
+            Toast.makeText(this, "Play Button Clicked", Toast.LENGTH_SHORT).show();
+        }
+        else if(view==btn_stop){
+            mediaPlayer.pause();
+            Toast.makeText(this, "Stop Button Clicked", Toast.LENGTH_SHORT).show();
+        }
     }
 }
